3e7b1c6201fc5560a738a22c8278c81af7f8e069
Estelo M Abellanosa <49965889+estelo2jz@users.noreply.github.com>
2020-04-17 12:34:58 -0500

    updated css

--- src/components/certifications.js
+++ src/components/certifications.js
@@ -7,12 +7,12 @@ export default class Certifications extends Component {
         <h1>Certifications</h1>
         <div className="certificate">
           <div className="certificate-one">
-            <h5>JavaScript</h5>
+            <h3>JavaScript</h3>
             <p>udemy.com/certificate/UC-d9e6ac4b-8b42-427d-84de-468066af8e74/</p>
           </div>
           <br />
           <div className="certificate-two">
-            <h5>ReactJS</h5>
+            <h3>ReactJS</h3>
             <p>udemy.com/certificate/UC-07624ZGK/</p>
           </div>
         </div>
--- src/components/header.js
+++ src/components/header.js
@@ -4,20 +4,21 @@ export default class Header extends Component {
   render() {
     return (
       <div className="header-container">
-        <h1>Header</h1>
         <div className="header-flex-container">
           <div className="header-title">
             <h1>Estelo Abellanosa</h1>
           </div>
 
-          <div className="header-links">
-            <p>linkedin.com/in/estelo-abellanosa-83a335186</p>
-          </div>
-          <div className="header-links">
-            <p>https://estelo-bottega-portfolio.herokuapp.com</p>
-          </div>
-          <div className="header-links">
-            <p>https://github.com/estelo2jz</p>
+          <div className="links-flex-container">
+            <div className="header-links">
+              <p>linkedin.com/in/estelo-abellanosa-83a335186</p>
+            </div>
+            <div className="header-links">
+              <p>https://estelo-bottega-portfolio.herokuapp.com</p>
+            </div>
+            <div className="header-links">
+              <p>https://github.com/estelo2jz</p>
+            </div>
           </div>
         </div>
       </div>
